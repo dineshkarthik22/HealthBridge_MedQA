e6f8a6ae029cd88419a05612888ee192b405dbd7
koichi-domoto <3190106004@zju.edu.cn>
2024-12-02 12:53:20 -0800

    add api for database url

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -11,10 +11,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1> Medical Q&A</h1>
-        
-        <div className="component-container">
-          <PDFUpload />
-        </div>
 
         <div className="component-container">
           <UploadDatabase />
--- src/api.py
+++ src/api.py
@@ -2,18 +2,24 @@ from flask import Flask, request, jsonify
 from flask_restx import Api, Resource, fields
 from flask_cors import CORS  # import CORS
 from response_generator import generate_response_for_subquery, generate_subqueries, evaluate_response
+from scraper_and_chunker import process_cleveland_clinic_page
 app = Flask(__name__)
-CORS(app)  #use CORS
+CORS(app)  # Use CORS
 api = Api(app, version='1.0', title='Patient Query API',
           description='A simple API to handle patient queries')
 
-# set query model
+# Set query model
 query_model = api.model('QueryModel', {
     'patient_id': fields.Integer(required=True, description='The ID of the patient'),
     'query': fields.String(required=True, description='The query text')
 })
 
-# api for user query
+# Set URL model
+url_model = api.model('UrlModel', {
+    'database_url': fields.String(required=True, description='The URL to process')
+})
+
+# API for user query
 @api.route('/query')
 class UserQuery(Resource):
     @api.expect(query_model)
@@ -30,11 +36,27 @@ class UserQuery(Resource):
         for subquery in subqueries:
             response = generate_response_for_subquery(subquery)
             evaluation = evaluate_response(response)
-            responses.append({'subquery': subquery, 'response': response, 'evaluation':  evaluation})
-    
+            responses.append({'subquery': subquery, 'response': response, 'evaluation': evaluation})
 
-        #responses = "succeed"  # sample
         return {'message': responses}, 200
+
+# API for processing URL
+@api.route('/process-url')
+class UrlProcessor(Resource):
+    @api.expect(url_model)
+    def post(self):
+        data = request.json
+        url = data.get('database_url')
+
+        if url is None or url == '':
+            return {'message': 'URL is required'}, 400
+        
+        # Simulate URL processing
+        try:
+            output_file = process_cleveland_clinic_page(url)
+            return {'message': 'Successfully processed page.'}, 200
+        except Exception as e:
+            return {'message': 'Error processing page: '+ str(e)}, 500
 
 if __name__ == '__main__':
     app.run(debug=True)
